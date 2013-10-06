2224901d518d5e50996db5561af9928433a47628
hgeg <alicanblbl@gmail.com>
2013-10-07 02:11:05 +0300

    fixes

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ from __future__ import with_statement
 import os,sys
 
 content = ""
-BASE_DIR = os.getcwd()
+BASE_DIR = sys.argv[1]
 HOME_DIR = os.path.expanduser("~")
-
 with open('runapps.sh','r') as f:
   content = f.read()
 with open('runapps.sh','w+') as f:
@@ -18,7 +17,7 @@ with open('settings.py','w+') as f:
 with open('rtorrent.rc','r') as f:
   content = f.read()
 with open('%s/.rtorrent.rc'%HOME_DIR,'w+') as f:
-  f.write(content.replace('<BASE_DIR',BASE_DIR))
+  f.write(content.replace('<BASE_DIR>',BASE_DIR))
 
 
 
