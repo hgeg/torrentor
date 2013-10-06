20c4589cbb4b87cb8162631618d7b27b136a269d
hgeg <alicanblbl@gmail.com>
2013-10-07 00:49:12 +0300

    spawn-fcgi prerequisites

--- install_prereq.sh
+++ install_prereq.sh
@@ -2,6 +2,8 @@ echo "rtorrent:"
 sudo apt-get install rtorrent 
 echo "pip:"
 sudo apt-get install python-pip
+echo "spawn-fcgi:"
+sudo apt-get install spawn-fcgi
 echo "screen:"
 sudo apt-get install screen
 echo "python/web.py:"
