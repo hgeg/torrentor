c169a2bc15f2ad7dba1745fef03cb94168c43fc3
hgeg <alicanblbl@gmail.com>
2013-10-07 01:24:34 +0300

    fixes

--- runapps.sh
+++ runapps.sh
@@ -1,4 +1,6 @@
+sudo killall rtorrent
+sudo killall core.py`
 echo "running rtorrent"
 screen -S rtorrent  -d -m rtorrent
 echo "running web layer"
-spawn-fcgi -d <BASE_DIR> -f <BASE_DIR>/core.py -a 127.0.0.1 -p 9595
+screen -S torrentor  -d -m ./core.py 
