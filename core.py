9940417a2681fb2e2a1176e21d9675937c482fbc
hgeg <alicanblbl@gmail.com>
2013-10-06 23:12:08 +0300

    deploy test

--- core.py
+++ core.py
@@ -1,15 +1,21 @@
-import web,requests,json,redis
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+from __future__ import with_statement
+import web,requests,json,redis,re,time,urllib
+import os,sys
+import settings
 
 urls = (
-  '/torrentor', 'Main',
-  '/torrentor/', 'Main',
-  '/torrentor/list/','List',
-  '/torrentor/media/', 'Media',
+  '/torrentor',             'Main',
+  '/torrentor/',            'Main',
+  '/torrentor/q/',          'Query',
+  '/torrentor/media:(.+)/', 'Media',
   #json views
-  '/torrentor/json:(.)+/', 'JSON'
+  '/torrentor/json:(.+)/',  'JSON'
 )
 
-render = web.template.render('templates')
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+render = web.template.render(settings.TEMPLATES_DIR)
 app = web.application(urls, globals())
 
 class Main:
@@ -18,24 +24,37 @@ class Main:
   def POST(self):
     return render.index()
 
-class List:
+class Query:
   def GET(self):
     return render.index()
   def POST(self):
     query = web.input().query
-    return render.list(query)
+    if re.match('(magnet:\?.*|http://.*/.*\.(torrent\?title=.*|torrent))',query):
+      timestamp = int(time.time())
+      with open('%s/tfile_%d.torrent'%(settings.TORRENTS_DIR,timestamp),'wb') as f:  
+        f.write(requests.get(query,stream=True).content)
+      return render.index()
+    else:
+      return render.list(query,[e for e in os.listdir(settings.MEDIA_DIR) if query in e.lower()])
 
 class Media:
-  def GET(self):
-    return render.media()
+  def GET(self,movie):
+    return render.media(movie)
   def POST(self):
     return render.index()
 
 class JSON:
-  def GET(self): pass
-  def POST(self): pass
-
+  def GET(self,call): pass
+  def POST(self,call):
+    web.header('Content-Type', 'application/json')
+    post = json.loads(web.data())
+    if call=='match':
+      query = post["query"];
+      found = [e for e in os.listdir(settings.MEDIA_DIR) if query in e.lower()]
+      if(found): return '[{"name":"%s","seeders":0,"leechers":0,"torrent":""}]'%found[0]
+      else: return requests.get('http://fenopy.se/module/search/api.php?keyword=%s&format=json&limit=1'%query).text
 
 if __name__ == '__main__':
-  app.internalerror = web.debugerror
+  #app.internalerror = web.debugerror
+  web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
   app.run()
--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 #Configuration
 
-BASE_DIR = "/home/can/Documents/torrentor"
+BASE_DIR = "<BASE_DIR>"
 
 TORRENTS_DIR  = "%s/torrents"%BASE_DIR
 DOWNLOADS_DIR = "%s/downloads"%BASE_DIR
-MEDIA_DIR     = "%s/media"%BASE_DIR
-TEMPLATES_DIR = "%s/media"%BASE_DIR
+MEDIA_DIR     = "%s/static/media"%BASE_DIR
+TEMPLATES_DIR = "%s/templates"%BASE_DIR
 
 
