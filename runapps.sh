487f512f17ef3f1849e83a46741586a46bc02bc0
hgeg <alicanblbl@gmail.com>
2013-10-07 01:07:48 +0300

    fixes

--- runapps.sh
+++ runapps.sh
@@ -1,4 +1,4 @@
 echo "running rtorrent"
 screen -S rtorrent  -d -m rtorrent
 echo "running web layer"
-screen -S torrentor  -d -m `spawn-fcgi -d <BASE_DIR> -f <BASE_DIR>/core.py -a 127.0.0.1 -p 9595`
+spawn-fcgi -d <BASE_DIR> -f <BASE_DIR>/core.py -a 127.0.0.1 -p 9595
