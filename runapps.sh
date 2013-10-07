9c4139177d640772c960528736f22f97b12056e2
hgeg <alicanblbl@gmail.com>
2013-10-07 15:34:35 +0300

    fixes

--- runapps.sh
+++ runapps.sh
@@ -3,9 +3,9 @@ sudo killall core.py
 echo "creating directiories"
 mkdir <DATA_DIR>/downloads
 mkdir <DATA_DIR>/finished
+mkdir <DATA_DIR>/media
 mkdir <SITE_DIR>/session
 mkdir <SITE_DIR>/torrents
-mkdir <SITE_DIR>/media
 echo "kill previous processes"
 sudo kill `pgrep -f rtorrent` 
 sudo kill `pgrep -f core.py` 
