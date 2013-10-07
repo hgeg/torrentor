8c44a10308760c0c279a5f5a4a65bf4727051c16
hgeg <alicanblbl@gmail.com>
2013-10-07 14:38:29 +0300

    fixes

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ from __future__ import with_statement
 import os,sys
 
 content = ""
-DATA_DIR = sys.argv[1]
-SITE_DIR = sys.argv[2]
+SITE_DIR = sys.argv[1]
+DATA_DIR = sys.argv[2]
 
 with open('runapps.sh','r') as f:
   content = f.read()
 with open('runapps.sh','w+') as f:
-  f.write(content.replace('<SITE_DIR>'SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
+  f.write(content.replace('<SITE_DIR>',SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
 
 with open('settings.py','r') as f:
   content = f.read()
 with open('settings.py','w+') as f:
-  f.write(content.replace('<SITE_DIR>'SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
+  f.write(content.replace('<SITE_DIR>',SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
 
 with open('%s/rtorrent.rc'%SETUP_DIR,'r') as f:
   content = f.read()
 with open('%s/.rtorrent.rc'%HOME_DIR,'w+') as f:
-  f.write(content.replace('<SITE_DIR>'SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
+  f.write(content.replace('<SITE_DIR>',SITE_DIR).replace('<DATA_DIR>',DATA_DIR))
