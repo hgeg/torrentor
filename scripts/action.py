44ddd03e34365354858fd8e51a78dee103271b70
hgeg <alicanblbl@gmail.com>
2013-10-30 00:25:03 +0200

    action script fix

--- scripts/action.py
+++ scripts/action.py
@@ -3,24 +3,27 @@
 import sys,shelve
 
 def init_list():
-  db = shelve.open("downloads.db")
+  db = shelve.open("downloads")
   db['list'] = []
   db.close()
+  return 0
 
 def remove_form_list(filename):
-  db = shelve.open("downloads.db")
+  db = shelve.open("downloads")
   lst = downloads['list']
   if filename in lst: lst.remove(filename)
   db.close()
+  return 0
 
 def add_to_list(filename):
-  db = shelve.open("downloads.db")
+  db = shelve.open("downloads")
   lst = downloads['list']
   lst.append(filename)
   lst.reverse()
   while len(lst)>10: lst.pop()
   lst.reverse()
   db.close()
+  return 0
 
 if __name__ == '__main__':
   if sys.argv[1] == '--add':
@@ -29,5 +32,4 @@ if __name__ == '__main__':
     remove_form_list(sys.argv[2])
   elif sys.argv[1] == '--init':
     init_list()
-  return 0
   
