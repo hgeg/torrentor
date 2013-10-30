64654e7171bc5b0795729f383f8aa3077fea2d04
hgeg <alicanblbl@gmail.com>
2013-10-30 15:28:26 +0200

    redis package name fix

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,5 +1,5 @@
 echo "rtorrent, pip, screen, redis, omxplayer:"
-sudo apt-get install rtorrent python-pip screen omxplayer redis
+sudo apt-get install rtorrent python-pip screen omxplayer redis-server
 echo "python/web.py:"
 sudo pip install web.py
 echo "python/redis:"
