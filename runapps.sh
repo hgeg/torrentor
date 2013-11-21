b14b09eff6b91ef09bdc948accb140ccd851d146
hgeg <alicanblbl@gmail.com>
2013-11-22 00:40:31 +0200

    fixes

--- runapps.sh
+++ runapps.sh
@@ -4,18 +4,39 @@ mkdir <DATA_DIR>/downloads
 #will add later
 #mkdir <DATA_DIR>/converting
 #mkdir <DATA_DIR>/finished
-mkdir <DATA_DIR>/media
-mkdir <SITE_DIR>/session
-mkdir <SITE_DIR>/torrents
+
+if [ -d "<DATA_DIR>/media" ]
+then
+  echo "media directory exists."
+else
+  mkdir <DATA_DIR>/media
+fi
+
+if [ -d "<DATA_DIR>/session" ]
+then
+  echo "session directory exists."
+else
+  mkdir <SITE_DIR>/session
+fi
+
+if [ -d "<DATA_DIR>/torrents" ]
+then
+  echo "torrents directory exists."
+else
+  mkdir <SITE_DIR>/torrents
+fi
+
 if [ ! -d static/media ]
 then                                                                                                                                                                             
   echo "linking media folder"
   ln -s <DATA_DIR>/media static/media
 fi
+
 echo "kill previous processes"
 ps ax | grep redis-server | cut -c 1-5 | sudo xargs kill
 ps ax | grep rtorrent | cut -c 1-5 | sudo xargs kill
 ps ax | grep core.py | cut -c 1-5 | sudo xargs kill
+
 echo "running redis"
 screen -S redis  -d -m redis-server
 echo "running rtorrent"
@@ -30,5 +51,6 @@ then
   sudo mv torrentor.conf /etc/nginx/sites-enabled/torrentor.conf
   sudo service nginx restart
 fi
+
 echo "initialize data"
 scripts/action.py --init 
--- setup.sh
+++ setup.sh
@@ -1,16 +1,27 @@
 echo "      Welcome to Torrentor.v2 installer      "
 echo "---------------------------------------------"
-echo "Trying to install dependencies..."
+echo ""
+echo "Installing dependencies..."
 echo ""
 
 ./install_prereq.sh
+
 echo "Finished. Now setting up config files..."
-echo ""
 python config.py $1 $2 
 mv .rtorrent.rc $HOME/.rtorrent.rc
-echo "Finished. Running services..."
+
 echo ""
+echo "Finished. Running services..."
 ./runapps.sh
+
+echo ""
+echo "cleaning up..."
+rm install_prereq.sh 
+rm runapps.sh
+rm config.py
+rm -- "$0"
+
 echo ""
 echo "---------------------------------------------"
-echo "For more info: gitub.com/hgeg/torrentor"
+echo "Installation finished."
+echo "For more info: hgeg.io/app/torrentor"
