46a97ec7fe1cd516a4891cad9375a89916ddcea2
Can Bülbül <alicanblbl@gmail.com>
2014-09-19 15:51:35 +0300

    v3

--- scripts/converter.sh
+++
@@ -1,5 +0,0 @@
-find finished -iname "*.avi" -o -iname "*.mkv" -exec mv {} /Users/can/Documents/torrentor/converting/ \;
-find finished -iname "*.mp4" -exec mv {} /Users/can/Documents/torrentor/static/media/ \;
-for i in /*.avi; do 
-  ffmpeg -i "<BASE_DIR>/converting/$i" -vcodec libx264 -b:v 1000k -ab 96k -ar 48k -f mp4 -strict experimental "<BASE_DIR>/static/media/$i"; 
-done
